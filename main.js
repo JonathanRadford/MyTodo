a219ab198852f8af27b045b1351bc7f92ea7faad
Jonathan Radford <jonathanradford@live.co.uk>
2021-11-24 16:58:51 +0000

    added screen flexibility

---
+++ main.js
@@ -0,0 +1,21 @@
+const input = document.querySelector(".input-box")
+const output = document.querySelector(".task-additions")
+const button = document.getElementById("btn-input")
+const deleteTask = document.querySelector(".btn-delete")
+const reset = document.querySelector(".btn-reset")
+//const addTask = document.querySelector("")
+
+button.addEventListener("click", () => {
+  const task = input.value
+  return output.innerHTML += `<div class="added-task"><input type="checkbox" class="checkbox"><h4 class="new-tasks">${task}</h4><button><i class="fas fa-pencil-alt btn-delete"></i></button></div>`;
+  })
+
+deleteTask.addEventListener("click", () => {
+   output.innerHTML += " ";
+})
+
+reset.addEventListener("click", () => {
+  input.value= " "
+  output.innerHTML = " "
+})
+  
